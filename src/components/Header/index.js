475543c2e44d52c362e689aa9c184a093331caf2
Rokas Augulis <rocky@Rokass-MacBook-Pro.local>
2020-10-06 08:16:10 +0300

    asdas

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.scss";
+
+function Header() {
+  return (
+    <div className="header_root">
+      <div className="header_root-button">
+        <button />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
